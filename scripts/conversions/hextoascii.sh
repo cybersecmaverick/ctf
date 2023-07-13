7b4e2c4ba06abbd165328e7d3c137c9febe83872
CyberSecurityMaverick <122697943+cybersecmaverick@users.noreply.github.com>
2023-07-13 22:43:41 +0100

    Update hextoascii.sh

--- scripts/conversions/hextoascii.sh
+++ scripts/conversions/hextoascii.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+################################################################################
+# Author: CyberSecMaverick
+# Date:26-06-2023
+#
+# Description: Reads hexadecimal values from a CSV file and converts them to ASCII.
+# Usage: ./hextoascii.sh <hex_values_file.csv>
+#
+# Required Files:
+#   - Hex Values File: Specify the hex values file as a command-line argument when running the script.
+#     Example: ./hextoascii.sh hex_values.csv
+#
+# Output:
+#   - The ASCII representation of the hexadecimal values, displayed on stdout.
+#
+################################################################################
+
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <hex_values_file.csv>"
     exit 1
 fi
 
+# Read each line from the hex values file
 while IFS=, read -r hex; do
+    # Extract the hexadecimal value and convert it to ASCII
     printf "\\x${hex:2:2}"
 done < "$1" | tr -d '\n'
